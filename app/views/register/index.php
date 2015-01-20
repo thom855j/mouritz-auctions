c5eaba22d4228bc7720613b08177d96dd50917f5
Jannik Viig <jannikviig@gmail.com>
2015-01-20 14:04:46 +0100

    Updated register

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,21 +1,24 @@
+<div class="page-wrapper" >
 
-<div class="container" style="width: 300px; text-align: center;">
+    <div class="login-wrap">
+     <?php
+    if (!empty($data->feedback)) {
+        foreach ($data->feedback as $feedback) {
+            echo $feedback;
+        }
+    }
+    ?>
+    <div class="heading">
+        <h2>Register</h2>
+    </div>
 
-    <form style=""class="form-signin" role="form" method="post" action="<?php echo URL; ?>register/verify" data-parsley-validate>
-        <p style="color: red;">
-
-            <?php
-            if (!empty($data->feedback)) {
-                foreach ($data->feedback as $feedback) {
-                    echo $feedback;
-                }
-            }
-            ?>
-        </p>
+    <form class="form-signin" role="form" method="post" action="<?php echo URL; ?>register/verify" data-parsley-validate>
         <input type="text" name="username"  class="form-control" placeholder="Brugernavn" required="" autofocus="">
         <input type="password" name="password"  class="form-control" placeholder="Adgangskode" required="">
-        <input class=" btn-lg btn-primary btn-block" type="submit" name="submit" value="Opret"><br>
-         <a href="<?php echo URL; ?>login">Login</a><br>
+        
+        <input class="btn-log" type="submit" name="submit" value="Register"><br>
+        <a href="<?php echo URL; ?>login">Login</a><br>
         <a href="<?php echo URL; ?>home">Back</a><br>
     </form>
+    <div> <!-- /login-wrap -->
 </div> <!-- /container -->
