e53c6af5213121f297b6123db0caffdc4a791f59
thom855j <thom855j@cvkweb.dk>
2015-01-20 09:17:15 +0100

    Global DB access

--- app/config/Databases.php
+++ app/config/Databases.php
@@ -8,7 +8,7 @@
 //CONNECTION
 if(DATABASE == 'thom855j') {
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '37.59.223.137');
 define('DB_NAME', 'thom855j_MouritzAuktioner');
 define('DB_USER', 'thom855j_sde');
 define('DB_PASS', 'sde101292');
